189c3e67c95d0a5b5106c00cfaa0c326f624c56a
awtershokk <awtershokk@gmail.com>
2024-03-21 17:36:56 +0700

    создание карты для выбора игра (максиму поправить)

--- frontend/src/pages/organizer/IndexOrganizer.js
+++ frontend/src/pages/organizer/IndexOrganizer.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
 import OrganizerHeader from '../../components/OrganizerHeader';
-
+import ImageCard from "../../components/ImageCard";
+import dota2Image from "../../img/dota2.png";
+import cs2Image from "../../img/cs2.jpeg"
+import warfaceImage from "../../img/warface.jpg"
 
 export default function IndexOrganizer() {
     return (
-            <div>
-                <OrganizerHeader />
-            </div>
+        <div>
+            <OrganizerHeader />
+            <ImageCard src={dota2Image} alt="Игра для умных"/>
+            <ImageCard src={cs2Image} alt="Игра для менее умных"/>
+            <ImageCard src={warfaceImage} alt="Игра для менее умных"/>
+
+        </div>
     );
 };
-
