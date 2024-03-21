efc7f4b96c282091d80c39c1d73a5ec8240e8f32
Maxim Kaspisky <90889536+suetakrsk@users.noreply.github.com>
2024-03-21 13:55:43 +0700

    Изменение внешнего вида входа и регистрации

--- frontend/src/pages/common/Login.js
+++ frontend/src/pages/common/Login.js
@@ -5,31 +5,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Login() {
     return (
-        <div className="Login-form-container">
-            <Form className="Login-form">
-                <div className="Login-form-content">
-                    <h3 className="Login-form-title">Авторизация</h3>
-                    <div className="text-center">
-                        Не зарегистрированы?{" "}
-                        <Link to="/register" className="link-primary">
-                            Зарегистрироваться
-                        </Link>
-                    </div>
-                    <Form.Group className="mt-3">
-                        <Form.Label>Электронная почта</Form.Label>
-                        <Form.Control type="email" />
-                    </Form.Group>
-                    <Form.Group className="mt-3">
-                        <Form.Label>Пароль</Form.Label>
-                        <Form.Control type="password" />
-                    </Form.Group>
-                    <div className="d-grid gap-2 mt-3">
-                        <Button type="submit" variant="primary">
-                            Войти
-                        </Button>
+        <div className="container d-flex justify-content-center align-items-center vh-100">
+            <div className="col-md-6">
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title text-center">Авторизация</h3>
+                        <div className="text-center mb-3">
+                            Не зарегистрированы?{" "}
+                            <Link to="/register" className="link-primary">
+                                Зарегистрироваться
+                            </Link>
+                        </div>
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Электронная почта</Form.Label>
+                                <Form.Control type="email" />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Пароль</Form.Label>
+                                <Form.Control type="password" />
+                            </Form.Group>
+                            <div className="d-grid gap-2">
+                                <Button type="submit" variant="primary">
+                                    Войти
+                                </Button>
+                            </div>
+                        </Form>
                     </div>
                 </div>
-            </Form>
+            </div>
         </div>
     );
 }
