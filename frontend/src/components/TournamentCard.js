0d0ffe676ea3d42ad9f1e012d2ea1f4ca519a024
Maxim Kaspisky <90889536+suetakrsk@users.noreply.github.com>
2024-03-22 00:47:34 +0700

    Изменение карточки турнира(исправление ошибки)

--- frontend/src/components/TournamentCard.js
+++ frontend/src/components/TournamentCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/TournamentCard.css';
-const TournamentCard = ({ title, discipline, status, date_satrt, date_end, teams }) => {
+const TournamentCard = ({ title, discipline, status, date_start, date_end, teams }) => {
     return (
         <div className="col-sm-6">
             <div className="card">
@@ -8,7 +8,7 @@ const TournamentCard = ({ title, discipline, status, date_satrt, date_end, teams
                     <h5 className="card-title">{title}</h5>
                     <h5 className="card-title">{discipline}</h5>
                     <h7 className="card-title status">{status}</h7>
-                    <h6 className="card-title date-range">{date_satrt} - {date_end}</h6>
+                    <h6 className="card-title date-range">{date_start} - {date_end}</h6>
                     <p className="card-text">{teams}</p>
                     <a href="#" className="btn btn-primary">Принять участие</a>
                 </div>
